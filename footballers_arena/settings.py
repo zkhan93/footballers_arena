8fd81913fdc0d90d27d774e08ad5bd6dd2baa172
Zeeshan Khan <zkhan19093@gmail.com>
2017-04-16 20:10:56 +0530

    empty static file

--- footballers_arena/settings.py
+++ footballers_arena/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 ]
 
 # Simplified static file serving.
