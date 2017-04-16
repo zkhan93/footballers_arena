ad9fcff877dc34e314bd33aa133c195b9e75504e
Zeeshan Khan <zkhan19093@gmail.com>
2017-04-16 20:09:51 +0530

    application is whitenoiseapplication now

--- footballers_arena/settings.py
+++ footballers_arena/settings.py
@@ -123,9 +123,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
